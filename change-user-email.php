c6563f0c780123f67c6714c82bf2e0ea2817cbd6
Henning Orth <mail@e-vance.net>
2017-04-26 17:02:25 +0200

    initial commit

--- change-user-email.php
+++ change-user-email.php
@@ -8,28 +8,31 @@ Author: E-VANCE / Henning Orth
 Author URI: https://www.e-vance.net
 */
 
-function wp_new_user_notification($user_id, $plaintext_pass = '') {
+if (!function_exists('wp_new_user_notification')) {
 
-  $user = get_userdata( $user_id );
-  $lang = pll_current_language();
+  function wp_new_user_notification($user_id, $plaintext_pass = '') {
 
-  // The blogname option is escaped with esc_html on the way into the database in sanitize_option
-  // we want to reverse this for the plain text arena of emails.
-  $blogname = wp_specialchars_decode(get_option('blogname'), ENT_QUOTES);
+    $user = get_userdata( $user_id );
+    $lang = pll_current_language();
 
-  $message  = sprintf(__('New user registration on your site %s:'), $blogname) . "\r\n\r\n";
-  $message .= sprintf(__('Username: %s'), $user->user_login) . "\r\n\r\n";
-  $message .= sprintf(__('E-mail: %s'), $user->user_email) . "\r\n";
+    // The blogname option is escaped with esc_html on the way into the database in sanitize_option
+    // we want to reverse this for the plain text arena of emails.
+    $blogname = wp_specialchars_decode(get_option('blogname'), ENT_QUOTES);
 
-  @wp_mail(get_option('admin_email'), sprintf(__('%s | New User registration'), $blogname), $message);
+    $message  = sprintf(__('New user registration on your site %s:'), $blogname) . "\r\n\r\n";
+    $message .= sprintf(__('Username: %s'), $user->user_login) . "\r\n\r\n";
+    $message .= sprintf(__('E-mail: %s'), $user->user_email) . "\r\n";
 
-  if ( empty($plaintext_pass) )
-    return;
+    @wp_mail(get_option('admin_email'), sprintf(__('%s | New User registration'), $blogname), $message);
 
-  $message  = sprintf(__( pll__('Registration Email Username') . ': %s'), $user->user_login) . "\r\n";
-  $message .= sprintf(__( pll__('Registration Email Password') . ': %s'), $plaintext_pass) . "\r\n";
-  $message .= pll__('Registration Email Message') . home_url() . "\r\n";
+    if ( empty($plaintext_pass) )
+      return;
 
-  wp_mail($user->user_email, sprintf(__('%s | '. pll__('Registration Email Title')), $blogname), $message);
+    $message  = sprintf(__( pll__('Registration Email Username') . ': %s'), $user->user_login) . "\r\n";
+    $message .= sprintf(__( pll__('Registration Email Password') . ': %s'), $plaintext_pass) . "\r\n";
+    $message .= pll__('Registration Email Message') . home_url() . "\r\n";
 
+    wp_mail($user->user_email, sprintf(__('%s | '. pll__('Registration Email Title')), $blogname), $message);
+
+  }
 }
